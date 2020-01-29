664cfd8bb13647dcd4aedcc0116ae8f38df2a7da
Anatolii Prylutskyi <anpryl@gmail.com>
2020-01-29 20:44:55 +0200

    Add err msg to debug logs

--- src/service/ratelimit.go
+++ src/service/ratelimit.go
@@ -141,7 +141,7 @@ func (this *service) ShouldRateLimit(
 			return
 		}
 
-		logger.Debugf("caught error during call")
+		logger.Debugf("caught error during call: %v", err)
 		finalResponse = nil
 		switch t := err.(type) {
 		case redis.RedisError:
